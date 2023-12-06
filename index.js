c5f92509408a62f67a806a4ad89bd24ded8b376d
Joao Santos <estudoti@hotmail.com>
2023-12-06 07:15:10 -0300

    calculo do multiplo

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const digit = process.argv[2];
+const number = 1000;
+const multiples = [];
 
-console.log(digit)
+if(!isNaN(digit)){
+    const v = parseInt(digit);
+    for (let n = 0; n < number; n++) {
+        if(n%v == 0){
+            multiples.push(n);
+        }
+    }
+    console.log(multiples);
+} else {
+    console.log('Não é um número!');
+}
