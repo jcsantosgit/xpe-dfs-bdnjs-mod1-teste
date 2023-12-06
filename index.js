4e83ed6ab2b04f2c50fba7ff2e51577ce9e9fa8b
Joao Santos <estudoti@hotmail.com>
2023-12-06 11:57:32 -0300

    escrevendo arquivos com FileStream

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-//const commons = require('./commonsJS.js');
-import operacoes from './operacoes.js';
+import fs from 'fs';
 
-const digit = process.argv[2];
-const digit2 = process.argv[3];
-
-const number = isNaN(digit2) ? 1000 : parseInt(digit2);
-const multiples = [];
-
-console.log(operacoes.soma(10, 10));
-
-if(!isNaN(digit)){
-    const v = parseInt(digit);
-    for (let n = 0; n <= number; n++) {
-        if(operacoes.ehMultiplo(n,v)){
-            multiples.push(n);
-        }
+fs.writeFile('arquito-teste.txt', "bla bla bla", function(err){
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("Arquivo criado com sucess!");
     }
-    console.log(multiples);
-} else {
-    console.log('Não é um número!');
-}
-
-// ERROS
-// Warning: To load an ES module, set "type": "module" in the package.json or use the .mjs extension.
+});
