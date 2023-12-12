6895f9c4f2f0c9b2f3003e51a634565e9fd7d2c8
Joao Santos <estudoti@hotmail.com>
2023-12-12 12:48:21 -0300

    trabalhando com parametros

--- index.js
+++ index.js
@@ -2,13 +2,40 @@ import express from 'express';
 
 const app = express();
 
-// Rotas:
-app.get("/", (req, res)=>{
-    res.send("GET: Hello world com express!");
+// Filtra qualquer método.
+// Parametros res sem utilização.
+app.all("/teste?", (_, res)=>{
+    res.send(req.method);
 });
 
-app.post("/", (req, res)=>{
+app.post("/incluir+", (req, res)=>{
     res.send("POST: Hello world com express")
+});
+
+// parâmetros opcionais
+app.get('/produto/:id/:qtd?', (req, res)=>{
+
+    var retorno = []
+
+    if(req.params.qtd !== undefined && req.params.qtd !== null)
+        retorno.push(req.params.qtd)
+    
+    if(req.params.id !== undefined && req.params.id !== null)
+        retorno.push(req.params.id)
+
+    res.send(retorno);
+});
+
+app.get('/produto/:id/:qtd')
+
+// Parâmetro com expressões regulares.
+app.get(/.*api*$/, (req, res) => {
+    res.send('usando API com expressão regular');
+});
+
+// Parametros via query
+app.get('/compra', (req, res)=>{
+    res.send(req.query);
 });
 
 // Subindo o servidor:
